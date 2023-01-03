fd3b58f3ac8b01e028c047d0809f511ae94255e7
Nihat <abdullazadenihat@gmail.com>
2023-01-03 21:23:51 +0400

    fixed nav bug

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,14 +2,31 @@ import { Link } from 'react-router-dom'
 
 import './Nav.scss'
 import githubImage from '../assets/img/github.svg'
+import hamburgerImage from '../assets/img/hamburger.svg'
 
 export default function Nav() {
   return (
-    <nav>
-      <div className="container">
-        <div className="row">
-          <h2 className="brand"> NLOG </h2>
-          <div className="link">
+    <header>
+      <nav className='nav'>
+        <div className="container">
+          <div className="row">
+            <h2 className="brand"> NLOG </h2>
+            <div className="link">
+              <ul>
+                <li> <Link to='/'> Home </Link>   </li>
+                <li> <Link to='/about'> About </Link>   </li>
+                <li> <a href="https://github.com/nihat-js"> <img src={githubImage} alt="" />   </a>   </li>
+              </ul>
+            </div>
+            <div className="hamburger">
+              <img src={hamburgerImage} alt="" />
+            </div>
+          </div>
+        </div>
+      </nav>
+      <nav className="mobile-nav d-none">
+        <div className="container">
+          <div className="row">
             <ul>
               <li> <Link to='/'> Home </Link>   </li>
               <li> <Link to='/about'> About </Link>   </li>
@@ -17,7 +34,9 @@ export default function Nav() {
             </ul>
           </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+    </header>
+
+
   )
 }
